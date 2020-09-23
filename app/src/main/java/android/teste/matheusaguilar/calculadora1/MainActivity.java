7262364903eadf69c9fb0745c62dd12fd4b05354
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-09-23 10:52:20 -0300

    Added Button Click Events

--- app/src/main/java/android/teste/matheusaguilar/calculadora1/MainActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/calculadora1/MainActivity.java
@@ -3,6 +3,11 @@ package android.teste.matheusaguilar.calculadora1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+
+import static java.lang.Double.parseDouble;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickButtonSum(View view){
+        EditText edt1 = (EditText) findViewById(R.id.valor1);
+        EditText edt2 = (EditText) findViewById(R.id.valor2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double valor1 = parseDouble(edt1.getText().toString());
+        double valor2 = parseDouble(edt2.getText().toString());
+
+        result.setText("Resultado: " + Double.toString(valor1 + valor2));
+    }
+
+    public void clickButtonSub(View view){
+        EditText edt1 = (EditText) findViewById(R.id.valor1);
+        EditText edt2 = (EditText) findViewById(R.id.valor2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double valor1 = parseDouble(edt1.getText().toString());
+        double valor2 = parseDouble(edt2.getText().toString());
+
+        result.setText("Resultado: " + Double.toString(valor1 - valor2));
+    }
+
+    public void clickButtonMult(View view){
+        EditText edt1 = (EditText) findViewById(R.id.valor1);
+        EditText edt2 = (EditText) findViewById(R.id.valor2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double valor1 = parseDouble(edt1.getText().toString());
+        double valor2 = parseDouble(edt2.getText().toString());
+
+        result.setText("Resultado: " + Double.toString(valor1 * valor2));
+    }
+
+    public void clickButtonDiv(View view){
+        EditText edt1 = (EditText) findViewById(R.id.valor1);
+        EditText edt2 = (EditText) findViewById(R.id.valor2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double valor1 = parseDouble(edt1.getText().toString());
+        double valor2 = parseDouble(edt2.getText().toString());
+
+        if(valor2 == 0.0)
+            result.setText("Erro: Divisao por zero!!!");
+        else
+            result.setText("Resultado: " + Double.toString(valor1 / valor2));
     }
 }
